8c2e71f35bd0560bc14beab0613c5265dcbfe4ca
Peter Geoghegan <pg@heroku.com>
2013-05-15 16:58:38 -0700

    Update to use new interface

--- lib/heroku-postgresql/client.rb
+++ lib/heroku-postgresql/client.rb
@@ -1,9 +1,9 @@
 class Heroku::Client::HerokuPostgresql
   def upgrade
-    http_post "#{@database_sha}/upgrade"
+    http_post "#{resource_name}/upgrade"
   end
 
   def upgrade_status
-    http_get "#{@database_sha}/upgrade_status"
+    http_get "#{resource_name}/upgrade_status"
   end
 end
